31df641ba14c2173a8423b2213eb92b70ee4b8ac
Adrian_Velazquez <adrian.velazquezo96@gmail.com>
2023-02-20 09:37:59 -0600

    [UPDATE] terminado

--- moviesProgramatically/Views/MovieList/MovieListView.swift
+++ moviesProgramatically/Views/MovieList/MovieListView.swift
@@ -90,7 +90,18 @@ extension MovieListView: MovieListViewUIDelegate {
     }
     
     func notifyMenuPressed() {
-        self.successFavoriteAdded()
+        let alertController = UIAlertController(title: "¿Que quieres hacer?", message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "Ver Perfil", style: .default) { _ in
+            self.successFavoriteAdded()
+        }
+        alertController.addAction(action1)
+        let action2 = UIAlertAction(title: "Desconectar", style: .destructive) { _ in
+            self.navigationController?.popViewController(animated: true)
+        }
+        alertController.addAction(action2)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     func notifyGenderSelected() {
